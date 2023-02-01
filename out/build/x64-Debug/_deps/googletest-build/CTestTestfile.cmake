2bd190eb48671d932d702ad2b3793d9c16fe20e8
Leonardo Santana <109393037+LeonardoSantanaA@users.noreply.github.com>
2023-02-01 10:56:39 -0300

    Add files via upload

--- out/build/x64-Debug/_deps/googletest-build/CTestTestfile.cmake
+++ out/build/x64-Debug/_deps/googletest-build/CTestTestfile.cmake
@@ -1,1 +1,7 @@
-
+# CMake generated Testfile for 
+# Source directory: C:/Users/lekao/Desktop/PhotoBatch/out/build/x64-Debug/_deps/googletest-src
+# Build directory: C:/Users/lekao/Desktop/PhotoBatch/out/build/x64-Debug/_deps/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
